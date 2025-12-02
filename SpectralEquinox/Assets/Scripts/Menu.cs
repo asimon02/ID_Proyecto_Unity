3f3c42ba0b2945d59f79a37b9026891b4e9e6c5f
Carmen Sanchez <csanchezda@alumnos.unex.es>
2025-12-02 17:26:00 +0100

    Menu with options

--- SpectralEquinox/Assets/Scripts/Menu.cs
+++ SpectralEquinox/Assets/Scripts/Menu.cs
@@ -3,6 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+
     public void StartGame()
     {
         //SceneManager.LoadScene("Level");
