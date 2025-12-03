52a7f86c018fdf960d67dcfcd1cf4546e5383e05
Saimon <92518360+asimon02@users.noreply.github.com>
2025-12-03 17:50:36 +0100

    Level1-Third-Commit

--- SpectralEquinox/Assets/Scripts/Player1.cs
+++ SpectralEquinox/Assets/Scripts/Player1.cs
@@ -2,7 +2,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player1 : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
     public float speed = 3f;
     private Rigidbody2D rb2D;
     private float move;
@@ -12,31 +12,52 @@ public class Player1 : MonoBehaviour {
     public float groundRadius = 0.1f;
     public LayerMask groundLayer;
     private Animator animator;
-    private int fuegosFatuos;
+    private static int fuegosFatuos;
     public TMP_Text textFuegoFatuo;
     public AudioSource audioSource;
     public AudioClip fuegoFatuoClip;
+    private bool isPlayer1;
+    private float playerScale;
 
     void Start() {
         rb2D = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        
+        if (gameObject.CompareTag("Player1")) {
+            isPlayer1 = true;
+            playerScale = 0.25f;
+        } else if (gameObject.CompareTag("Player2")) {
+            isPlayer1 = false;
+            playerScale = 0.5f;
+        }
     }
 
     void Update() {
         move = 0;
 
-        if (Input.GetKey(KeyCode.A)) move = -1;
-        if (Input.GetKey(KeyCode.D)) move = 1;
+        if (isPlayer1) {
+            // Player1: A, W, D
+            if (Input.GetKey(KeyCode.A)) move = -1;
+            if (Input.GetKey(KeyCode.D)) move = 1;
+            
+            if (Input.GetKeyDown(KeyCode.W) && isGrounded) {
+                rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
+            }
+        } else {
+            // Player2: Flechas
+            if (Input.GetKey(KeyCode.LeftArrow)) move = -1;
+            if (Input.GetKey(KeyCode.RightArrow)) move = 1;
+            
+            if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded) {
+                rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
+            }
+        }
 
         rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
 
         if(move != 0) {
-            float scaleX = Mathf.Sign(move) * 0.25f;
-            transform.localScale = new Vector3(scaleX, 0.25f, 0.25f);
-        }
-
-        if (Input.GetKeyDown(KeyCode.W) && isGrounded) {
-            rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpForce);
+            float scaleX = Mathf.Sign(move) * playerScale;
+            transform.localScale = new Vector3(scaleX, playerScale, playerScale);
         }
 
         animator.SetFloat("Speed", Mathf.Abs(move));
@@ -49,7 +70,6 @@ public class Player1 : MonoBehaviour {
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
-
         if(collision.transform.CompareTag("Coin")) {
             audioSource.PlayOneShot(fuegoFatuoClip);
             Destroy(collision.gameObject);
